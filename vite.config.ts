a1dd611516c04a484dc8d28deb4e25f8a05474bf
Niklas Gittel <niklas.gittel@gmail.com>
2025-09-13 17:40:25 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "talkdeeply-landing",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
