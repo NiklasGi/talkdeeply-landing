6e6ac94a6ae39623cf254f991725b511dbf8ab88
Niklas Gittel <niklas.gittel@gmail.com>
2025-11-04 20:37:48 +0100

    does it get more important than this?

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -5,7 +5,7 @@ const MOBILE_BREAKPOINT = 768
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
-  
+
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
@@ -16,6 +16,7 @@ export function useIsMobile() {
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
+  
   return !!isMobile
 }
  
